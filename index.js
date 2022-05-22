02a8237ae7358b1d582e1bbf8fe7f28c369e4ca4
emu_yuu <mhk2.747.vclv@gmail.com>
2022-05-22 17:48:16 +0900

    init

---
+++ index.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const sceneEl = document.querySelector("a-scene");
+
+  const list = ["spiderman", "mask", "chicken", "pikachu"];
+  let visible = 0;
+  const setNextVisible = () =>
+    visible < list.length - 1 ? visible++ : (visible = 0);
+
+  sceneEl.addEventListener("targetFound", () => {
+    [...Array(list.length).keys()].forEach((_, i) => {
+      const entity = document.querySelector(`#${list[i]}-entity`);
+      i === visible
+        ? entity.setAttribute("visible", true)
+        : entity.setAttribute("visible", false);
+    });
+  });
+
+  sceneEl.addEventListener("targetLost", () => {
+    setNextVisible();
+  });
+});
